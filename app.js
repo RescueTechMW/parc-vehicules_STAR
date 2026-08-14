6fe379b65208a30b2a3cb437be6c18df363d33aa
SemaineFamille <melanie.weil.81@gmail.com>
2026-08-14 10:05:09 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const vehicule =
+document.getElementById("vehicule");
+
+vehicule.value =
+localStorage.getItem("vehicule")
+|| "413";
+
+vehicule.addEventListener("change", () => {
+
+    localStorage.setItem(
+        "vehicule",
+        vehicule.value
+    );
+
+});
+
+function enregistrer(){
+
+    const km =
+    document.getElementById("kilometres").value;
+
+    document.getElementById(
+        "message"
+    ).innerHTML =
+    "✅ " +
+    vehicule.value +
+    " : " +
+    km +
+    " km enregistré";
+
+}
