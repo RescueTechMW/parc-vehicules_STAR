aa91abeeb76d2877ffc0cf66db7ddd9569de2a31
SemaineFamille <melanie.weil.81@gmail.com>
2026-08-21 17:24:04 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-console.log("ADMIN VERSION 21-08-2026 17h07");
+console.log("ADMIN VERSION 21-08-2026 17h27");
 
 const URL_APPS_SCRIPT =
 "https://script.google.com/macros/s/AKfycbwx9_RWV5PfuLBtjpT0C_VNYQoc604fwTGZhC2Jl0nfYI8debfm6i-Sroka81JCJYdW/exec";
@@ -42,6 +42,8 @@ async function chargerDashboard() {
                 item =>
                 item[2].toString() === v.toString()
             );
+        console.log("vehicule recherché :", v);
+console.log("ligne trouvée :", ligne);
 
         if(ligne){
 
