b476a450f907a4b6a83d2d5b56e5f9e096c287df
SemaineFamille <melanie.weil.81@gmail.com>
2026-08-22 10:19:54 +0200

    Update factures.js

--- factures.js
+++ factures.js
@@ -1,17 +1,15 @@
-console.log("APP VERSION 22-08-2026 10h05");
+console.log("APP VERSION 22-08-2026 10h20");
 
 const vehicules = {
-
-    "413":"VD 452753",
-    "414":"VD 2632",
-    "415":"VD 57590",
-    "416":"VD 57601",
-    "417":"VD 451038",
-    "418":"VD 57687",
-    "419":"VD 261616",
-    "Logistique":"VD 395957",
-    "SMART":"VD 57580"
-
+    "413": "VD 452753",
+    "414": "VD 2632",
+    "415": "VD 57590",
+    "416": "VD 57601",
+    "417": "VD 451038",
+    "418": "VD 57687",
+    "419": "VD 261616",
+    "Logistique": "VD 395957",
+    "SMART": "VD 57580"
 };
 
 const selectVehicule =
@@ -19,12 +17,6 @@ document.getElementById("vehicule");
 
 const selectPlaque =
 document.getElementById("plaque");
-
-for(const plaque of Object.values(vehicules)){
-
-    selectPlaque.innerHTML +=
-    `<option>${plaque}</option>`;
-}
 
 selectVehicule.addEventListener(
     "change",
@@ -46,13 +38,14 @@ selectPlaque.addEventListener(
         Object.keys(vehicules)
         .find(
             key =>
-            vehicules[key]
-            ===
+            vehicules[key] ===
             selectPlaque.value
         );
 
-        selectVehicule.value =
-        vehicule;
+        if(vehicule){
+            selectVehicule.value =
+            vehicule;
+        }
 
     }
 );
