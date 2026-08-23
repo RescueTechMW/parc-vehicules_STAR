83c5a4c508d6e5709522185af41035df92fc9d49
SemaineFamille <melanie.weil.81@gmail.com>
2026-08-23 13:50:58 +0200

    Create historique-factures.js

---
+++ historique-factures.js
@@ -0,0 +1,63 @@
+const URL_APPS_SCRIPT =
+"https://script.google.com/macros/s/AKfycbwx9_RWV5PfuLBtjpT0C_VNYQoc604fwTGZhC2Jl0nfYI8debfm6i-Sroka81JCJYdW/exec";
+
+async function chargerHistorique(){
+
+    const response =
+    await fetch(
+        URL_APPS_SCRIPT +
+        "?action=historiqueFactures"
+    );
+
+    const data =
+    await response.json();
+
+    const container =
+    document.getElementById(
+        "historiqueContainer"
+    );
+
+    container.innerHTML = "";
+
+    data
+    .slice(1)
+    .reverse()
+    .forEach(f => {
+
+        const date =
+        new Date(f[0])
+        .toLocaleDateString("fr-CH");
+
+        container.innerHTML += `
+        <div class="vehicule-card card-ok">
+
+            <div class="card-header">
+
+                <span>${f[3]}</span>
+
+                <span>
+                CHF ${f[6]}
+                </span>
+
+            </div>
+
+            <div class="card-km">
+                ${f[7]}
+            </div>
+
+            <div class="card-date">
+                ${f[5]}
+            </div>
+
+            <div class="card-date">
+                ${date}
+            </div>
+
+        </div>
+        `;
+
+    });
+
+}
+
+chargerHistorique();
